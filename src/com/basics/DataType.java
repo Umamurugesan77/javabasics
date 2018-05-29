b18dd6261b10b175a252b989a628cd71ee1a4488
Umamurugesan77 <umadurka1998@gmail.com>
2018-05-29 06:08:22 -0700

    sample datatype program

---
+++ src/com/basics/DataType.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class DataType {
+	public static void main(String arg[])
+	{
+		byte one = 4;
+		byte two = 6;
+		int three = (int)one + (int)two;
+		System.out.println(three);
+	}
+
+}
