de686f348921bfcdc645b7df4a6d00fea5f79826
Umamurugesan77 <umadurka1998@gmail.com>
2018-05-29 06:18:49 -0700

    count

---
+++ src/com/basics/Datatype1.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+public class Datatype1 {
+	public static void main(String args[])
+	{
+		int count = 15;
+		count++;
+		count--;
+		System.out.println(count > 15);
+		System.out.println(count < 15);
+	}
+
+}
